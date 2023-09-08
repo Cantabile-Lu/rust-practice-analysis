44cbadea138189897e8f7965b06bdc674a50d61d
luwenhai99999 <834299862@qq.com>
2023-09-08 23:54:57 +0800

    fix: unit-06

--- code/unit-06/src/main.rs
+++ code/unit-06/src/main.rs
@@ -1,3 +1,38 @@
+/* 1 
 fn main() {
-    println!("Hello, world!");
+    let s: &str = "hello, world";
 }
+*/
+
+/* 2. 
+// 1 
+// fn main() {
+//     let s: Box<str> = "hello, world".into();
+//     greetings(&s)
+// }
+
+// fn greetings(s: &str) {
+//     println!("{}",s)
+// }
+
+// 2: 
+// fn main() {
+//     let s:&str = "hello, world";
+//     greetings(s)
+// }
+
+// fn greetings(s: &str) {
+//     println!("{}",s)
+// }
+*/
+
+/* 3: 
+fn main() {
+    let mut s = String::new();
+    s.push_str("hello, world");
+    s.push('!');
+
+    // println!("{s}");
+    assert_eq!(s, "hello, world!");
+}
+*/
