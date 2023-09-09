d2f2389158505f09b11c3e05ba61c4eec5b6f7ee
luwenhai99999 <834299862@qq.com>
2023-09-10 01:28:44 +0800

    fix: unit-06

--- code/unit-06/src/main.rs
+++ code/unit-06/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
 }
 */
 
-/* 2. 
+/* 2.
 // 1 
 fn main() {
     let s: Box<str> = "hello, world".into();
@@ -15,18 +15,19 @@ fn greetings(s: &str) {
     println!("{}",s)
 }
 
-2: 
+// 2: 
 fn main() {
-    let s:&str = "hello, world";
+    let s: Box<str> = "hello, world".into();
+    println!("{s}")
     greetings(s)
 }
 
-fn greetings(s: &str) {
+fn greetings(s: Box<str>) {
     println!("{}",s)
 }
-*/
+ */
 
-/* 3: */
+/* 3: 
 fn main() {
     let mut s = String::with_capacity(10);
     let mut s = String::from("");
@@ -39,7 +40,7 @@ fn main() {
     assert_eq!(s, "hello, world!");
 }
 
-
+*/
 
 /* 4. 
 fn main() {
@@ -63,3 +64,98 @@ fn main() {
     println!("{}", s)
 }
 */
+
+/* 5.
+fn main() {
+    let s = String::from("I like dogs");
+    // 以下方法会重新分配一块内存空间，然后将修改后的字符串存在这里
+    let s1 = s.replace("dogs", "cats");
+
+    assert_eq!(s1, "I like cats")
+} */
+
+/* 6.
+fn main() {
+    let s1 = String::from("hello,");
+    let s2 = String::from("world!");
+    let s3 = s1.clone() + &s2; 
+    assert_eq!(s3,"hello,world!");
+    println!("{}",s1);
+} */
+
+/* 7. 
+
+fn main() {
+    let s = "hello, world".into();
+    greetings(s)
+}
+
+fn greetings(s: String) {
+    println!("{}",s)
+}
+
+fn main() {
+    let s = "hello, world";
+    greetings(s)
+}
+
+fn greetings(s: &str) {
+    println!("{}",s)
+}
+*/
+
+/* 8. 
+fn main() {
+    // 1: 
+    let s = "hello, world".to_string();
+    let s1: &str = &s;
+    println!("{s1}")
+
+    // 2: 
+    let s = "hello, world";
+    let s1: &str = s;
+    println!("{s1}")
+}
+*/
+
+/* 9. 
+fn main() {
+    // 你可以使用转义的方式来输出想要的字符，这里我们使用十六进制的值，例如 \x73 会被转义成小写字母 's'
+    // 填空以输出 "I'm writing Rust"
+    let byte_escape = "I'm writing Ru\x73\x74!";
+    println!("What are you doing\x3F (\\x3F means ?) {}", byte_escape);
+
+    // 也可以使用 Unicode 形式的转义字符
+    let unicode_codepoint = "\u{211D}";
+    let character_name = "\"DOUBLE-STRUCK CAPITAL R\"";
+
+    println!("Unicode character {} (U+211D) is called {}",
+                unicode_codepoint, character_name );
+
+    // // 还能使用 \ 来连接多行字符串
+    let long_string = "String literals\
+                        can span multiple lines.\
+                        The linebreak and indentation here \
+                         can be escaped too!";
+    println!("{}", long_string);
+}*/
+
+/* 10 */
+fn main() {
+    let raw_str = r"Escapes don't work here: \x3F \u{211D}";
+    println!("{raw_str}")
+    // 修改上面的行让代码工作
+    // assert_eq!(raw_str, "Escapes don't work here: ? ℝ");
+
+    // 如果你希望在字符串中使用双引号，可以使用以下形式
+    // let quotes = r#"And then I said: "There is no escape!""#;
+    // println!("{}", quotes);
+
+    // // 如果希望在字符串中使用 # 号，可以如下使用：
+    // let  delimiter = r###"A string with "# in it. And even "##!"###;
+    // println!("{}", delimiter);
+
+    // // 填空
+    // let long_delimiter = __;
+    // assert_eq!(long_delimiter, "Hello, \"##\"")
+}
--- unit-06.md
+++ unit-06.md
@@ -19,6 +19,9 @@ fn main() {
 
 需要注意的是：
 
+- `into`是一个常用的转换方法，它将一个类型转换成另一个类型。
+- 函数形参标注的类型就决定了函数实参的类型。
+
 正确代码如下：
 
 ```rust
@@ -33,13 +36,15 @@ fn greetings(s: &str) {
 }
 // 2: 
 fn main() {
--   let s: Box<str> = "hello, world".into();
-+   let s:&str = "hello, world";
+    let s: Box<str> = "hello, world".into();
     greetings(s)
 }
-fn greetings(s: &str) {
-    println!("{}",s)
-}
+-fn greetings(s: &str) {
+-    println!("{}",s)
+-}
++fn greetings(s: Box<str>) {
++    println!("{}",s)
++}
 ```
 
 3. 🌟
@@ -94,6 +99,109 @@ fn main() {
 +    s.push_str(" world");
 +	 s += "!";
     println!("{}", s)
+}
+```
+
+5. 🌟🌟
+
+正确代码如下：
+
+```rust
+fn main() {
+    let s = String::from("I like dogs");
+    // 以下方法会重新分配一块内存空间，然后将修改后的字符串存在这里
+-    let s1 = s._("dogs", "cats");
++    let s1 = s.replace("dogs", "cats");
+    assert_eq!(s1, "I like cats")
+}
+```
+
+6. 🌟🌟
+
+需要注意的是： 为什么`s2`不能与`s1`调换位置？
+
+正确代码如下：
+
+```rust
+fn main() {
+    let s1 = String::from("hello,");
+    let s2 = String::from("world!");
+-    let s3 = s1 + s2; 
++    let s3 = s1.clone() + &s2; 
+    assert_eq!(s3,"hello,world!");
+    println!("{}",s1);
+}
+```
+
+7. 🌟🌟
+
+正确代码如下：
+
+```rust
+// 1
+fn main() {
+-    let s = "hello, world";
++    let s = "hello, world".into();
+    greetings(s)
+}
+
+fn greetings(s: String) {
+    println!("{}",s)
+}
+// 2
+fn main() {
+    let s = "hello, world";
+    greetings(s)
+}
+
+-fn greetings(s: String) {
+-    println!("{}",s)
+-}
+
++fn greetings(s: &str) {
++    println!("{}",s)
++}
+```
+
+8. 🌟🌟
+
+正确代码如下：
+
+```rust
+fn main() {
+    // 1: 
+    let s = "hello, world".to_string();
+    let s1: &str = &s;
+
+    // 2: 
+    let s = "hello, world";
+    let s1: &str = s;
+}
+```
+
+9. 🌟
+
+正确代码如下：
+
+```rust
+fn main() {
+    // 你可以使用转义的方式来输出想要的字符，这里我们使用十六进制的值，例如 \x73 会被转义成小写字母 's'
+    // 填空以输出 "I'm writing Rust"
+    let byte_escape = "I'm writing Ru\x73\x74!";
+    println!("What are you doing\x3F (\\x3F means ?) {}", byte_escape);
+
+    // 也可以使用 Unicode 形式的转义字符
+    let unicode_codepoint = "\u{211D}";
+    let character_name = "\"DOUBLE-STRUCK CAPITAL R\"";
+
+    println!("Unicode character {} (U+211D) is called {}",
+                unicode_codepoint, character_name );
+    // // 还能使用 \ 来连接多行字符串
+    let long_string = "String literals\
+                        can span multiple lines.\
+                        The linebreak and indentation here \
+                         can be escaped too!";
+    println!("{}", long_string);
 }
 ```
 
