17e48dfa81cf816e4cffa59a78468dbcaac78651
luwenhai99999 <834299862@qq.com>
2023-09-15 23:33:01 +0800

    fix: unit-06-12

--- code/unit-06/src/main.rs
+++ code/unit-06/src/main.rs
@@ -185,13 +185,12 @@ fn main() {
 */
 
 /* 12 */
-
 fn main() {
     // 填空，打印出 "你好，世界" 中的每一个字符
     for c in "你好，世界".chars() {
         println!("{}", c)
     }
     let word = "你好，世界";
-    let chars = word.chars();
-    println!("{:?}", chars)
+    let mut chars = word.chars();
+    assert_eq!(Some('你'), chars.next());
 }
